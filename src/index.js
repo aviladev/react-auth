5d16ab063e45b2d325185600ec6fa6b0ecb9b10e
Pedro Ávila <pedroavila95@gmail.com>
2018-02-25 21:00:48 -0300

    Add redux and redux-form

--- src/index.js
+++ src/index.js
@@ -5,15 +5,23 @@ import {
   Route,
   Switch
 } from 'react-router-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+import reducers from './reducers'
 
 import App from './components/App'
 
+const store = createStore(reducers)
+
 ReactDOM.render(
-  <Router>
-    <Switch>
-      <Route path='/' component={App} />
-    </Switch>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route path='/' component={App} />
+      </Switch>
+    </Router>
+  </Provider>
   , document.getElementById('root')
 )

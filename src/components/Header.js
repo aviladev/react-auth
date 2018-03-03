1840dc6d18e1ee9c31b60a79b72c92a2ad6cb87f
Pedro Ávila <pedroavila95@gmail.com>
2018-03-03 16:25:17 -0300

    Update Header to toggle signin/signup/signout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,58 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 class Header extends Component {
+  renderLinks = () => this.props.authenticated
+    ? (
+      <li className='nav-item'>
+        <NavLink
+          className='btn btn-outline-warning ml-2'
+          to='/signout'
+        >
+            Sign Out
+        </NavLink>
+      </li>
+    )
+    : (
+      <Fragment>
+        <li className='nav-item'>
+          <NavLink
+            to='/signin'
+            className='btn btn-outline-success ml-2'
+          >
+            Sign In
+          </NavLink>
+        </li>
+
+        <li className='nav-item'>
+          <NavLink
+            to='/signup'
+            className='btn btn-outline-success ml-2'
+          >
+            Sign Up
+          </NavLink>
+        </li>
+      </Fragment>
+    )
+
   render () {
     return (
       <nav className='navbar navbar-expand-lg navbar-light bg-light mb-3'>
         <div className='container'>
+          <NavLink to='/' className='navbar-brand'>React Auth</NavLink>
 
           <ul className='nav navbar-nav'>
-            <li className='nav-item'>
-              <NavLink to='/signin'
-                className='btn btn-outline-success my-2 my-sm-0'
-              >
-                Sign In
-              </NavLink>
-            </li>
+            {this.renderLinks()}
           </ul>
-
         </div>
       </nav>
     )
   }
 }
 
-export default Header
+const mapStateToProps = ({
+  auth: { authenticated }
+}) => ({ authenticated })
+
+export default connect(mapStateToProps)(Header)
